c9f2dc1ecf48631bf2ac041118208631462e788c
Abraomukas <a.hernandezv.g@gmail.com>
2022-09-26 02:03:18 +0400

    feat: create a new account case

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,45 +11,61 @@ router.post('/:userId', (req, res) => {
 	const { userId } = req.params;
 	const { initialCredit } = req.body;
 
-	console.log('user - '.toLocaleUpperCase() + userId);
-
-	//* Find the user
-	//* We assume it exists already
-	UserModel.findOne({ userId: userIdToFind })
+	//* Find the user - (We assume it exists already)
+	UserModel.findOne({ userId: userId })
 		.then((user) => {
 			console.log('user found!'.toLocaleUpperCase());
-
-			console.log('USER - ' + user);
-			console.log('ID - ' + user.userId);
 
 			//* New account
 			const account = new AccountModel({
 				accountId: Math.floor(Math.random() * 100) + '',
+				credit: 0,
+				transactions: []
 			});
 
-			account
-				.save()
-				.then((newAccount) => {
-					console.log('a new account has been opened!'.toLocaleUpperCase());
-					console.log('');
+			user.accounts.push(account.accountId);
+
+			//* Transaction check
+			if (initialCredit > 0) {
+				console.log('a new transaction has occurred!'.toLocaleUpperCase());
+				const transaction = new TransactionModel({
+					transactionId: Math.floor(Math.random() * 100) + '',
+					credit: initialCredit,
+				});
+
+				account.credit += transaction.credit;
+				account.transactions.push(transaction.transactionId);
+
+				user.balance += account.credit;
+
+				transaction.save()
+					.then(() => { console.log("new transaction created!".toLocaleUpperCase()); })
+					.catch((txnErr) => {
+						console.log('transaction save - error:'.toLocaleUpperCase(), txnErr);
+						res.status(500).send({ message: 'Transaction save error...', txnErr });
+					})
+			}
+
+			account.save()
+				.then(() => {
+					console.log("new account created!".toLocaleUpperCase());
+				}).catch((accErr) => {
+					console.log('account save - error:'.toLocaleUpperCase(), accErr);
+					res.status(500).send({ message: 'Account save error...', accErr });
 				})
-				.catch((err) => {
-					res.status(500).send({ message: 'Account creation error...', err });
+
+			user.save()
+				.then(() => {
+					res.status(201).send({ message: "A new account has been created!" });
+				}).catch((userErr) => {
+					console.log('user update - error:'.toLocaleUpperCase(), userErr);
+					res.status(500).send({ message: 'User update error...', userErr });
 				});
 		})
 		.catch((err) => {
 			console.log('user not found...'.toLocaleUpperCase(), err);
 			res.status(404).send({ message: 'User not found...', err });
 		});
-
-	//* Transaction check
-	if (initialCredit > 0) {
-		const transaction = new TransactionModel({
-			credit: initialCredit,
-		});
-	}
-
-	res.status(201).send({ message: '' });
 });
 
 //* User Details
